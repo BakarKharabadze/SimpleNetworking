ec43230e4381c051210b34abbb989d71b4e44d4d
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-05-01 17:20:02 +0400

    Networking

--- Package.swift
+++ Package.swift
@@ -12,12 +12,8 @@ let package = Package(
             targets: ["Networking"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+
         .target(
-            name: "Networking"),
-        .testTarget(
-            name: "NetworkingTests",
-            dependencies: ["Networking"]),
+            name: "Networking")
     ]
 )
--- Sources/Networking/Networking.swift
+++ Sources/Networking/Networking.swift
@@ -1,2 +1,37 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case invalidResponse
+    case decodingError
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func request<T: Decodable>(url: String, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        guard let url = URL(string: url) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            do {
+                let decodedObject = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedObject))
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+}
